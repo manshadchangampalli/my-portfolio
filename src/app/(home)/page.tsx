7d24850451b5fa414a54b85948de77ff4c3f50c5
Manshad Changampalli <manshadchangampalli010@gmail.com>
2025-09-28 21:11:38 +0400

    feat: images added in hero section with gsap scrolling

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "./_components/hero/Hero";
+
+export default function Home() {
+  return (
+    <main className="bg-white w-full h-[200vh]">
+      <Hero />
+      <div className="h-[200vh] bg-red-500"></div>
+    </main>
+  );
+}
