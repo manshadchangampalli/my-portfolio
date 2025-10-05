ebcca0164a17ce5fb14fbf1940d9842df40c1505
Manshad Changampalli <manshadchangampalli010@gmail.com>
2025-10-05 13:06:10 +0400

    fix: the last two sections removed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -12,12 +12,7 @@ export default function Home() {
       <div className="h-screen bg-gray-100 flex items-center justify-center relative">
         <h2 className="text-4xl font-bold text-gray-800"></h2>
       </div>
-      <div className="h-screen bg-blue-100 flex items-center justify-center">
-        <h2 className="text-4xl font-bold text-blue-800"></h2>
-      </div>
-      <div className="h-screen bg-green-100 flex items-center justify-center">
-        <h2 className="text-4xl font-bold text-green-800">Even more content</h2>
-      </div>
+
     </main>
   );
 }
