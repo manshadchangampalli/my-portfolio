5efc475409f3d3d08b325269021a4196a8e6dc8d
Manshad Changampalli <manshadchangampalli010@gmail.com>
2025-10-11 23:34:35 +0400

    the small animation and experience page start

--- src/app/(home)/_components/hero/HeroProfile.tsx
+++ src/app/(home)/_components/hero/HeroProfile.tsx
@@ -1,16 +1,19 @@
 "use client";
 
+import { cn } from "@/utils/classNames";
 import LiquidGlass from "react-ios-liquid-glass";
 import "react-ios-liquid-glass/dist/index.css";
+import useDevice from "@/hooks/useDevice";
 
 export default function HeroProfile() {
+    const { isMobile } = useDevice();
     return (
         <div className="fixed grid items-center justify-center z-20 left-0 top-0 w-full min-h-screen">
             <div
                 style={{ transform: "perspective(1000px)" }}
                 className="rounded-sm sm:rounded-2xl hero_profile_bar text-white flex items-center sm:w-[80vw] w-[90vw] min-h-[200px] sm:min-h-[300px]">
                 <LiquidGlass
-                    className="hero_profile_bar_liquid"
+                    className={cn("hero_profile_bar_liquid ", isMobile && 'bg-white text-black')}
                     padding="0px"
                     borderRadius="0px"
                     width="100%"
--- src/app/(home)/_components/second-section/SecondSection.tsx
+++ src/app/(home)/_components/second-section/SecondSection.tsx
@@ -3,11 +3,12 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Image from "next/image";
 import React, { useRef } from "react";
 
 const SecondSection = () => {
     const sectionRef = useRef<HTMLDivElement | null>(null);
-    const panelsRef = useRef<HTMLDivElement[]>([]);
+    const panelsRef = useRef<HTMLDivElement | null>(null);
 
     gsap.registerPlugin(ScrollTrigger);
 
@@ -15,26 +16,24 @@ const SecondSection = () => {
         const section = sectionRef.current;
         const panels = panelsRef.current;
 
-        if (!section || panels.length === 0) return;
-
-        // Reset all panels
-        gsap.set(panels, { opacity: 0, xPercent: 100 });
-        gsap.set(panels[0], { opacity: 1, xPercent: 0 });
-
         // ScrollTrigger configuration
         const tl = gsap.timeline({
             scrollTrigger: {
                 trigger: section,
-                start: "top top",             // start when section hits top of viewport
-                end: () => "+=" + window.innerHeight * (panels.length - 1),
+                start: "top top", // start when section hits top of viewport
+                end: () => "+=" + window.innerHeight,
                 scrub: true,
                 pin: true,
                 anticipatePin: 1,
-                markers: false,               // set true for debugging positions
+                markers: false, // set true for debugging positions
             },
         });
-
-
+        tl.to(panels, {
+            opacity: 1,
+            xPercent: 0,
+            ease: "none",
+            duration: 1,
+        });
 
         return () => {
             ScrollTrigger.getAll().forEach((st) => st.kill());
@@ -46,9 +45,23 @@ const SecondSection = () => {
         <div className="bg-[#585955] section_two">
             <div
                 ref={sectionRef}
-                className="min-h-screen w-screen flex items-center justify-center relative overflow-hidden"
-            >
-
+                className="min-h-screen w-screen  relative overflow-hidden">
+                <header className="absolute px-6 py-4 items-start flex w-full justify-between h-20">
+                    <h1 className="text-[48px] font-bold">EXPERIENCES</h1>
+                    <span className="text-[18px] font-light">AUG 2022 - AUG 2023</span>
+                </header>
+                <main ref={panelsRef} className="flex flex-col opacity-0 items-center pt-10 justify-center w-full h-screen">
+                    <Image
+                        src="/images/experiences/confiancelabs.png"
+                        alt="experience-1"
+                        width={250}
+                        height={250}
+                    />
+                    <p className="text-center max-w-[70vw] text-[24px] font-[300]">
+                        As a freelance developer, I transformed client visions into high-performing digital experiences, delivering scalable web and mobile solutions with
+                        precision, creativity, and modern technology.
+                    </p>
+                </main>
             </div>
         </div>
     );
